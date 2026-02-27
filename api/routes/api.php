8d011faa3a85628b28da7691f1dd9254fb03b320
Miquel <miqueldurangalarzo@gmail.com>
2026-02-27 11:07:13 +0100

    auth

---
+++ api/routes/api.php
@@ -0,0 +1,8 @@
+<?php
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
